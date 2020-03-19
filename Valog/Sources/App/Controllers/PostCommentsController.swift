2aa81ea9dca3102b4fc2f447a8aa8c70388a1a57
Harley-xk <harley.gb@foxmail.com>
2020-03-19 08:58:52 +0800

    评论接口调整：增加身份认证、倒序排列、统计评论数目

--- Valog/Sources/App/Controllers/PostCommentsController.swift
+++ Valog/Sources/App/Controllers/PostCommentsController.swift
@@ -22,7 +22,9 @@ final class PostCommentsController: RouteCollection {
     
     func boot(routes: RoutesBuilder) throws {
         routes.get("posts", ":id", "comments", use: index)
-        routes.post("posts", ":id", "comments", use: create)
+        
+        routes.grouped(Token.authenticator().middleware())
+            .post("posts", ":id", "comments", use: create)
     }
     
     func index(_ request: Request) throws -> EventLoopFuture<Page<PostComment.Public>> {
@@ -34,7 +36,7 @@ final class PostCommentsController: RouteCollection {
             .with(\.$sender)
             .with(\.$replyTo)
             .filter(\.$post.$id == postId)
-            .sort(\.$createdAt, .ascending)
+            .sort(\.$createdAt, .descending)
             .paginate(for: request).map { (page) -> (Page<PostComment.Public>) in
                 return page.map { $0.makePublic() }
         }
@@ -51,11 +53,11 @@ final class PostCommentsController: RouteCollection {
                 let body = try request.content.decode(PostCommentCreatingBody.self)
                 if let user = request.auth.get(User.self) {
                     // 已登录用户，直接创建评论
-                    return try self.createComment(on: request, for: postId, with: body, sender: user)
+                    return try self.createComment(on: request, for: post, with: body, sender: user)
                 } else if let sender = body.sender {
                     return try User.fetchTourist(on: request, for: sender)
                         .flatMapThrows { user in
-                            return try self.createComment(on: request, for: postId, with: body, sender: user)
+                            return try self.createComment(on: request, for: post, with: body, sender: user)
                     }
                 } else {
                     throw Abort(.badRequest)
@@ -65,10 +67,12 @@ final class PostCommentsController: RouteCollection {
     
     private func createComment(
         on request: Request,
-        for postId: String,
+        for post: Post,
         with body: PostCommentCreatingBody,
         sender: User
     ) throws -> EventLoopFuture<PostComment.Public> {
+        // 文章评论数 + 1
+        post.comments += 1
         if let replyTo = body.replyTo {
             return PostComment.find(replyTo, on: request.db).unwrap(or: Abort(.notFound))
                 .flatMapThrows { (replied) -> EventLoopFuture<PostComment> in
@@ -78,14 +82,16 @@ final class PostCommentsController: RouteCollection {
                     }
                     return request.eventLoop.future(replied)
             }.flatMap { (replied) -> EventLoopFuture<PostComment.Public> in
-                let comment = PostComment(postId: postId, content: body.content, sender: sender, replyTo: replied)
-                return comment.create(on: request.db)
-                    .transform(to: comment.loadEagerAndMakePublic(on: request))
+                let comment = PostComment(postId: post.id!, content: body.content, sender: sender, replyTo: replied)
+                return comment.create(on: request.db).flatMapThrows {
+                    post.update(on: request.db)
+                }.transform(to: comment.loadEagerAndMakePublic(on: request))
             }
         } else {
-            let comment = PostComment(postId: postId, content: body.content, sender: sender, replyTo: nil)
-            return comment.create(on: request.db)
-                .transform(to: comment.loadEagerAndMakePublic(on: request))
+            let comment = PostComment(postId: post.id!, content: body.content, sender: sender, replyTo: nil)
+            return comment.create(on: request.db).flatMapThrows {
+                post.update(on: request.db)
+            }.transform(to: comment.loadEagerAndMakePublic(on: request))
         }
     }
     
